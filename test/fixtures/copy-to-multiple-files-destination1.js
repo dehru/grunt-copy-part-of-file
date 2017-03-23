8542a7807d2374a6e02fac44ddbf2228e70aa562
Chiara Attard <chiara.attard@ixaris.com>
2017-03-23 12:29:52 +0100

    Updated test fixture files to represent an example of a destination file.

--- test/fixtures/copy-to-multiple-files-destination1.js
+++ test/fixtures/copy-to-multiple-files-destination1.js
@@ -1,6 +1,5 @@
 var testString = "test string";
 
 var testFunction = function() {
-	var destinationOneArray = ['1'];
-	return destinationOneArray;
+	return testString;
 };
--- test/fixtures/copy-to-multiple-files-destination2.js
+++ test/fixtures/copy-to-multiple-files-destination2.js
@@ -1,6 +1,5 @@
 var testString = "test string";
 
 var testFunction = function() {
-	var destinationTwoArray = ['1', '2'];
-	return destinationTwoArray;
+	return testString;
 };
